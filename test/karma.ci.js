64d98d159bd5d820ae5ba8f643b91b3c42f0b27b
zen_n <zenhuangzheng@126.com>
2017-04-17 18:20:25 +0800

    test ci

--- test/karma.ci.js
+++ test/karma.ci.js
@@ -88,13 +88,10 @@ module.exports = function (config) {
     },
     customLaunchers,
     frameworks: ['mocha', 'sinon-chai', 'source-map-support'],
-    files: [
-      './entry.js', // 这是测试入口文件
-      '../src/component/**/*.js' // 这是需要计算代码覆盖率的文件
-    ],
+    files: ['./entry.js'],// 这是测试入口文件
+    port: 9876,
     preprocessors: {
-      './entry.js': ['webpack', 'sourcemap'],
-      '../src/component/**/*.js' : ['webpack', 'sourcemap', 'coverage']
+      './entry.js': ['webpack', 'sourcemap', 'coverage']
     },
     reporters: ['spec', 'coverage', 'saucelabs'],
     singleRun: true,
