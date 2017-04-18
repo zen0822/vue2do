0a07285b28ea303ca09ee46e394a5262c9030646
zen_n <zenhuangzheng@126.com>
2017-04-18 12:01:57 +0800

    test ci

--- test/karma.ci.js
+++ test/karma.ci.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const config = require('./config.json')
 const webpackConf = require('../build/config/base.webpack.conf')(config.appName)
+const customLaunchers = require('./launcher.sauceLab.json')
 
 delete webpackConf.entry
+
 module.exports = function (config) {
   process.env.SAUCE_USERNAME = 'zen_n'
   process.env.SAUCE_ACCESS_KEY = 'ab082b18-8c48-4378-be1a-2f85059acc71'
@@ -10,68 +12,6 @@ module.exports = function (config) {
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
     console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
     process.exit(1)
-  }
-
-
-  const customLaunchers = {
-    sl_chrome_36: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '36'
-    },
-    sl_chrome_46: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '46'
-    },
-    sl_chrome_56: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 10',
-      version: '56'
-    },
-    sl_firefox_30: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '30'
-    },
-    sl_firefox_50: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '50'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 10',
-      version: '11'
-    },
-    sl_ie_10: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8',
-      version: '10'
-    },
-    sl_ie_9: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '9'
-    },
-    sl_safari: {
-      base: 'SauceLabs',
-      browserName: 'iPhone 4s Simulator',
-      platform: 'iOS',
-      version: '8.1'
-    },
-    sl_android: {
-      base: 'SauceLabs',
-      browserName: 'android',
-      platform: 'Linux',
-      version: '4.4'
-    }
   }
 
   config.set({
