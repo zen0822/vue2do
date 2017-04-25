2b78fb789e068b85b11bcec2094da08f72e20420
zen_n <zenhuangzheng@126.com>
2017-04-25 17:42:03 +0800

    [build] 0.0.2

---
+++ build/rel.sh
@@ -0,0 +1,28 @@
+set -e
+
+if [[ -z $1 ]]; then
+  echo "Enter new version: "
+  read VERSION
+else
+  VERSION=$1
+fi
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Releasing $VERSION ..."
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+  if [[ -z $RELEASE_TAG ]]; then
+    npm publish
+  else
+    npm publish --tag $RELEASE_TAG
+  fi
+fi
