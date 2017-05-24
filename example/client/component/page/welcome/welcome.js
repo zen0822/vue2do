00dd1d5d59136edfe233bdf6f3e06a9379d44acf
zen_n <zenhuangzheng@126.com>
2017-05-24 17:39:21 +0800

    更新主页

--- example/client/component/page/welcome/welcome.js
+++ example/client/component/page/welcome/welcome.js
@@ -1,11 +1,18 @@
 import './welcome.scss'
-import template from './welcome.tpl'
+import pug from './welcome.pug'
 import tip from 'src/component/base/pop/tip'
 import alert from 'src/component/base/pop/alert'
+import colComp from 'src/component/common/layout/col/col'
+import rowComp from 'src/component/common/layout/row/row'
 import mixin from '../component/mixin'
 
 export default {
-  template,
+  template: pug(),
+
+  components: {
+    column: colComp,
+    row: rowComp
+  },
 
   mixins: [mixin],
 
--- example/client/component/page/welcome/welcome.tpl
+++
@@ -1,6 +0,0 @@
-<div class="welcome">
-  <div class="welcome-bg">
-    <h3 class="z-text-center z-m-t-double">welcome to</h3>
-    <h2 class="z-text-center z-m-t-double">vue2do</h2>
-  </div>
-</div>
