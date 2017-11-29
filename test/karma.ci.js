77ba7fc73c747da2f6472b3f1c94bc2ec6febc85
zen_n <zenhuangzheng@126.com>
2017-11-29 16:01:59 +0800

    修改集成测试配置文件

--- test/karma.ci.js
+++ test/karma.ci.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
     },
     port: 9877,
     reporters: ['spec', 'coverage'],
-    singleRun: false,
+    singleRun: true,
     webpack: webpackConf,
     webpackMiddleware: {
       noInfo: true
