35860b8bde50521f810b914461a0ebb9d46c2fee
zen_n <zenhuangzheng@126.com>
2018-10-26 20:44:47 +0800

    master：优化发布流程

--- build/release.sh
+++ build/release.sh
@@ -13,23 +13,28 @@ fi
 read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Releasing $VERSION ..."
+if [[ $VERSION == $CURRENT_VERSION ]]; then
+  echo "The version of '$VERSION' and the online package version cannot be equal."
+  exit
+else
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Releasing $VERSION ..."
 
-  # build production
-  npm run prod
+    # build production
+    npm run prod
 
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
-  git pull
+    # commit
+    git add -A
+    git commit -m "[build] $VERSION"
+    npm version $VERSION --message "[release] $VERSION"
+    git pull
 
-  # publish
-  git push
-  if [[ -z $RELEASE_TAG ]]; then
-    npm publish
-  else
-    npm publish --tag $RELEASE_TAG
+    # publish
+    git push
+    if [[ -z $RELEASE_TAG ]]; then
+      npm publish
+    else
+      npm publish --tag $RELEASE_TAG
+    fi
   fi
 fi
