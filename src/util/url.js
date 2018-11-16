f8b8bba6ca06c6df41890f88e61e74849facef11
zen_n <zenhuangzheng@126.com>
2018-11-16 15:00:17 +0800

    vue2do-9:增加新的 url 工具函数

--- src/util/url.js
+++ src/util/url.js
@@ -27,6 +27,29 @@ const search = (urlSearch) => {
   return searchObj
 }
 
+/**
+ * 去掉 url 搜索参数里面多余的形参
+ *
+ * xxx.com?a=&b=2&c= => xxx.com?b=2
+ *
+ * @param {string} urlSearch - url 中的 search 值
+ */
+const fitSearch = (urlSearch) => {
+  const urlSearchObj = search(urlSearch)
+  const urlSearchObjKeys = Object.keys(urlSearchObj)
+  const urlSearchObjValues = Object.keys(urlSearchObj)
+  let urlSearchArr = []
+
+  urlSearchObjValues.forEach((item, index) => {
+    if (item !== '') {
+      urlSearchArr.push(`${urlSearchObjKeys[index]}=${item}`)
+    }
+  })
+
+  return urlSearchArr.join('&')
+}
+
 export {
-  search
+  search,
+  fitSearch
 }
