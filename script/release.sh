3d38219eaf6138c8280fa53a3d344759b9fe47ad
zen_n <zenhuangzheng@126.com>
2019-03-26 14:49:17 +0800

    [build] 0.4.10

--- script/release.sh
+++ script/release.sh
@@ -31,6 +31,7 @@ else
     npm version $VERSION --message "[release] $VERSION"
 
     # publish
+    git push origin refs/tags/v"$VERSION"
     git push
     if [[ -z $RELEASE_TAG ]]; then
       npm publish
