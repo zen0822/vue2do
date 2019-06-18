8a58776b70d3b945965796088c6387e02aceee58
zen_n <zenhuangzheng@126.com>
2019-06-18 21:57:13 +0800

    vue2do-44: 修复 test:ci 脚本

--- test/karma.ci.js
+++ test/karma.ci.js
@@ -1,55 +1,14 @@
-/**
- * 使用 mocha 测试框架 http://mochajs.org/
- * chai 的 测试断言框架 http://chaijs.com/api/bdd/#method_a
- */
-
-const path = require('path')
-const config = require('./config.json')
-const merge = require('webpack-merge')
-const webpackBaseConf = require('../script/config/base.webpack.conf')({
-  appName: config.appName
-})
-
-const webpackConf = merge(webpackBaseConf, {
-  module: {
-    rules: [{
-      test: /\.js$|\.jsx$/,
-      use: {
-        loader: 'istanbul-instrumenter-loader'
-      },
-      include: path.resolve('./unit/'),
-      exclude: /node_modules|\.spec\.js$/
-    }]
-  }
-})
-
-delete webpackConf.entry
-delete webpackConf.optimization
+const karmaConf = require('./karma.config')
 
 module.exports = function (config) {
   config.set({
-    basePath: '../',
-    browsers: ['Chrome'],
-    captureTimeout: 120000,
-    colors: true,
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-      dir: path.join(__dirname, 'coverage')
-    },
-    frameworks: ['mocha', 'sinon-chai', 'source-map-support'],
-    files: [
-      './test/unit/**/*.test.js'
-    ],
-    preprocessors: {
-      './test/unit/**/*.test.js': ['webpack', 'sourcemap']
-    },
-    port: 9877,
-    reporters: ['spec', 'coverage-istanbul'],
+    ...karmaConf,
     singleRun: true,
-    webpack: webpackConf,
-    webpackMiddleware: {
-      noInfo: true
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     }
   })
 }
